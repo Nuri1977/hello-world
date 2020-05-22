7c2c0ba4936ad00ae0a6c7a2c3831750006d2e63
nuri1977 <nurilacka@gmail.com>
2020-05-22 21:23:55 +0200

    interpolation

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -2,20 +2,36 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: '[app-product]',
-  template: `<div>
-              Inline template for products
-            </div>`,
+  template: `
+    <div>Welcome {{name}}</div>
+    <div>{{name.length}}</div>
+    <div>{{name.toLocaleUpperCase()}}</div>
+    <h3>{{greetUser()}}</h3>
+    <h2>{{siteUrl}}</h2>
+
+            
+  `,
   styles: [` 
     div {
       color: red;
+    }
+
+    h2 {
+      color: blue;
     }
   `]
 })
 export class ProductComponent implements OnInit {
 
+  public name = "vishwas";
+  public siteUrl = window.location.href;
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  greetUser() {
+    return "Hello " + this.name;
   }
 
 }
