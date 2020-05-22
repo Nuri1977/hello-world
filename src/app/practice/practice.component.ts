6f4d4bdc31a08f2c1eba8e722b46cb8cac6422dc
nuri1977 <nurilacka@gmail.com>
2020-05-23 00:15:31 +0200

    ngIf directive

--- src/app/practice/practice.component.ts
+++ src/app/practice/practice.component.ts
@@ -6,7 +6,17 @@ import { Component, OnInit } from '@angular/core';
     <hr>
     <input [(ngModel)]="name" type="text">
     {{name}}
+    <hr>
     <h5>Structural Directives (ad/remove HTML elements)</h5>
+    <div *ngIf="displayName; then thenBlock; else elseBlock"></div>
+
+    <ng-template #thenBlock>
+      <h4>ngIf directive </h4>
+    </ng-template>
+
+    <ng-template #elseBlock>
+      <h4>ngIf directive is hidden</h4>
+    </ng-template>
   
   `,
   styles: [`
@@ -16,6 +26,7 @@ import { Component, OnInit } from '@angular/core';
 export class PracticeComponent implements OnInit {
 
   public name="";
+  displayName = false;
 
   constructor() { }
 
