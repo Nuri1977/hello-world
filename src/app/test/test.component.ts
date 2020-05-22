294788ff58fa00cecef71881156da6a2972e9559
nuri1977 <nurilacka@gmail.com>
2020-05-22 21:45:21 +0200

    property binding

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -2,11 +2,17 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-test',
-  templateUrl: './test.component.html',
+  template: `
+    <input [id]="myId"  type="text" value="Nuri" class="m-3">
+    <input [disabled]="isDisabled" id={{myId}}  type="text" value="Nuri" class="m-3">
+    <input bind-disabled="isDisabled" id={{myId}}  type="text" value="Nuri" class="m-3">
+  `,
   styleUrls: ['./test.component.css']
 })
 export class TestComponent implements OnInit {
 
+  public myId = "testId";
+  public isDisabled = true;
   constructor() { }
 
   ngOnInit(): void {
