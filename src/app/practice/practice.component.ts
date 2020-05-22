153a10901cbc756a7b75e91f0ae4d9788ed4dd2b
nuri1977 <nurilacka@gmail.com>
2020-05-23 00:25:06 +0200

    ngIf switch directive

--- src/app/practice/practice.component.ts
+++ src/app/practice/practice.component.ts
@@ -4,20 +4,24 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-practice',
   template: `
     <hr>
-    <input [(ngModel)]="name" type="text">
-    {{name}}
+      <input [(ngModel)]="name" type="text">
+      {{name}}
     <hr>
-    <h5>Structural Directives (ad/remove HTML elements)</h5>
-    <div *ngIf="displayName; then thenBlock; else elseBlock"></div>
-
-    <ng-template #thenBlock>
-      <h4>ngIf directive </h4>
-    </ng-template>
-
-    <ng-template #elseBlock>
-      <h4>ngIf directive is hidden</h4>
-    </ng-template>
-  
+      <h1>Structural Directives (ad/remove HTML elements)</h1>
+      <div *ngIf="displayName; then thenBlock; else elseBlock"></div>
+      <ng-template #thenBlock>
+        <h4>ngIf directive </h4>
+      </ng-template>
+      <ng-template #elseBlock>
+        <h4>ngIf directive is hidden</h4>
+      </ng-template>
+    <hr>
+      <div [ngSwitch] = "color">
+        <div *ngSwitchCase="'red'">You picked red color</div>
+        <div *ngSwitchCase="'blue'">You picked blue color</div>
+        <div *ngSwitchCase="'green'">You picked green color</div>
+        <div *ngSwitchDefault>Pick again</div>
+      </div>
   `,
   styles: [`
   
@@ -27,6 +31,7 @@ export class PracticeComponent implements OnInit {
 
   public name="";
   displayName = false;
+  public color= "orange";
 
   constructor() { }
 
