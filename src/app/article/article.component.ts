2edfddad9b773b2e04eea2912e55fc773bfc61a6
nuri1977 <nurilacka@gmail.com>
2020-05-25 13:40:21 +0200

    16 pipes

--- src/app/article/article.component.ts
+++ src/app/article/article.component.ts
@@ -7,8 +7,17 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 })
 export class ArticleComponent implements OnInit {
 
+
   @Input('parentData') public name1;
   @Output() public childEvent = new EventEmitter();
+  public emri = "PipeExample";
+  public mesazh = "Welcome to Pipes";
+  public personazh = {
+    "firstName": "Adelina",
+    "secondName": "Ismaili"
+  }
+  public numer = 10000.55555;
+  public date = new Date();
 
   constructor() { }
 
